5575b16f97f68f0c3d7057deaf9434d2a2ebc214
Jmrxr <jairenriquez30@gmail.com>
2024-11-01 08:36:42 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Header from "./components/Header";
 import { GlobalProvider } from "./context/GlobalState";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import TransactionForm from "./components/TransactionForm";
 
 function App() {
   return (
     <GlobalProvider>
       <Header />
+      <Balance />
+      <TransactionForm />
       <h1>App react</h1>
     </GlobalProvider>    
   )
